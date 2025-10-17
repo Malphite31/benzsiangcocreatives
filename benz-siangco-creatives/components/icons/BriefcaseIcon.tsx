b386c4f3e092dc112080e444748ec9ba490999cf
Malphite31 <75958093+Malphite31@users.noreply.github.com>
2025-10-18 01:43:26 +0800

    Delete benz-siangco-creatives directory

--- benz-siangco-creatives/components/icons/BriefcaseIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-export const BriefcaseIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.07a2.25 2.25 0 0 1-2.25 2.25H5.92a2.25 2.25 0 0 1-2.25-2.25v-4.07a2.25 2.25 0 0 1 2.25-2.25h1.094c.757 0 1.41.312 1.88.83l.08.082a2.7 2.7 0 0 0 3.82 0l.08-.082a2.252 2.252 0 0 1 1.88-.83h1.094a2.25 2.25 0 0 1 2.25 2.25Z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.094m5.376-1.012a2.252 2.252 0 0 1 1.88.83l.08.082a2.7 2.7 0 0 0 3.82 0l.08-.082a2.252 2.252 0 0 1 1.88-.83M15.75 9h-6" />
-  </svg>
-);
